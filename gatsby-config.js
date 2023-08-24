36ced3fd1e82950ac1be217e2796c45ee31da166
jaehyeonlee <jerry0608s@gmail.com>
2023-08-24 22:18:58 +0900

    Feat: ESLint와 Prettier 설정 및 Typescript 설정
    ESLint와 Prettier 설정 및 Typescript 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,4 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
@@ -15,26 +9,15 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        isTSX: true,
+        allExtensions: true,
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
   ],
 }
