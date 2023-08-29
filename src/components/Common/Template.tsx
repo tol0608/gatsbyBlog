f2482715850764c2d489b2461e99d61f4a872cc2
jaehyeonlee <jerry0608s@gmail.com>
2023-08-29 10:40:49 +0900

    Docs: "수정, 제거 메타태그"
    Template.tsx 수정
    text.tsx 제거
    구글 메타 태그 html 제거

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -30,6 +30,15 @@ const Template: FunctionComponent<TemplateProps> = function ({
       <Helmet>
         <title>{title}</title>
 
+        <meta
+          name="google-site-verification"
+          content="QofucJpegXJ3d0nCtNg59LCCzfT4ZMx5PnG1Wk1ojyc"
+        />
+        <meta
+          name="naver-site-verification"
+          content="e0465db6d2e9c5165fa764004e83fa1cc06e56f8"
+        />
+
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
@@ -47,15 +56,6 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:image" content={image} />
         <meta name="twitter:site" content="@jaehyeon" />
         <meta name="twitter:creator" content="@jaehyeon" />
-
-        <meta
-          name="google-site-verification"
-          content="QofucJpegXJ3d0nCtNg59LCCzfT4ZMx5PnG1Wk1ojyc"
-        />
-        <meta
-          name="naver-site-verification"
-          content="e0465db6d2e9c5165fa764004e83fa1cc06e56f8"
-        />
 
         <html lang="ko" />
       </Helmet>
